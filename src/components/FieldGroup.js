d17a0d0daf46c2d70df8551ba5dbd39b18067ea6
Steve Burtenshaw <steve.burtenshaw@gmail.com>
2019-03-24 15:56:55 +0000

    Split out classNames

--- src/components/FieldGroup.js
+++ src/components/FieldGroup.js
@@ -17,7 +17,12 @@ export const FieldGroup = ({
     )}
     <Field
       className={classNames(
-        'bg-transparent ba b--black-20 pa3 mb3 w-100',
+        'bg-transparent',
+        'ba',
+        'b--black-20',
+        'pa3',
+        'mb3',
+        'w-100',
         fieldClassName
       )}
       id={props.name}
