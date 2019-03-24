dcc097dec3ef88d14cfe45f417ae13aea508cedb
Steve Burtenshaw <steve.burtenshaw@gmail.com>
2019-03-24 15:55:31 +0000

    Split AddModal into individual components

---
+++ src/components/AddModalContent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { AddForm } from './AddForm';
+
+export const AddModalContent = ({ closeModal, dispatch }) => {
+  return (
+    <div
+      style={{
+        background: 'white',
+        borderRadius: 10,
+        padding: 50,
+        maxWidth: 825,
+        width: '100%',
+      }}
+    >
+      <AddForm
+        handleSubmit={item => dispatch({ type: 'addItem', payload: item })}
+        closeModal={closeModal}
+      />
+    </div>
+  );
+};
